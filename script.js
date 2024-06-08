4957872746202adbebc66030ce66b20e57461277
Silvio Aguilar <96831040+silvio-aguilar@users.noreply.github.com>
2024-06-08 12:55:54 +0200

    fixError && Cursos

--- script.js
+++ script.js
@@ -11,30 +11,3 @@ document.getElementById('toggle-menu').addEventListener('click', function() {
 
 
 
-// Script para manejar el cambio de tema
-const themeToggleBtn = document.getElementById('theme-toggle');
-const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-
-// Cambiar los iconos dependiendo del tema actual
-if (localStorage.getItem('theme') === 'dark') {
-  document.documentElement.classList.add('dark');
-  themeToggleLightIcon.classList.remove('hidden');
-} else {
-  themeToggleDarkIcon.classList.remove('hidden');
-}
-
-themeToggleBtn.addEventListener('click', () => {
-  // Alternar iconos
-  themeToggleDarkIcon.classList.toggle('hidden');
-  themeToggleLightIcon.classList.toggle('hidden');
-
-  // Si el modo oscuro está activado, desactívalo, de lo contrario, actívalo
-  if (document.documentElement.classList.contains('dark')) {
-    document.documentElement.classList.remove('dark');
-    localStorage.setItem('theme', 'light');
-  } else {
-    document.documentElement.classList.add('dark');
-    localStorage.setItem('theme', 'dark');
-  }
-});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'selector',
-  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class', // Asegúrate de que esto esté configurado correctamente
+  content: ["./src/**/*.{html,js}"], // Asegúrate de que esto apunte a tus archivos correctamente
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'green-400': '0 4px 6px -1px rgba(34, 197, 94, 0.5), 0 2px 4px -2px rgba(34, 197, 94, 0.3)',
+      },
+    },
   },
   plugins: [],
 }
-
